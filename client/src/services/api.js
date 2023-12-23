7b4973aedca1eb515bca88f9222b3d01005c2150
gauravmekhe <100957597+gauravmekhe@users.noreply.github.com>
2023-12-23 16:26:42 +0530

    vercelapi

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://mobile-ordering-app.vercel.app/',
   headers:{
     "Content-Type": "application/json"
 }
